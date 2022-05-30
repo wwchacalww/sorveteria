2aad6b71e3a48bd00ba63693f041ac7908f75b81
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-05-30 09:57:23 -0300

    Atualizando entidade e tsconfig

--- src/product/domain/entities/product.spec.ts
+++ src/product/domain/entities/product.spec.ts
@@ -1,18 +1,38 @@
 import { Product, ProductProps } from "./product";
 describe("Product Unit Test", () => {
   test("construct of product", () => {
-    const props: ProductProps = {
-      barcode: "123456789",
-      category: "picole",
-      created_at: new Date(),
-      description: "test description",
-      is_active: true,
+    let props: ProductProps = {
       name: "test",
-      updated_at: new Date(),
+      description: "test description",
+      category: "picole",
     };
 
-    const product = new Product(props);
+    let product = new Product(props);
 
-    expect(product.props).toStrictEqual(props);
+    expect(product.props.created_at).toBeInstanceOf(Date);
+    props.created_at = new Date();
+    props.updated_at = new Date();
+    product = new Product(props);
+    expect(product.props).toStrictEqual({
+      name: "test",
+      description: "test description",
+      category: "picole",
+      barcode: "",
+      is_active: true,
+      created_at: props.created_at,
+      updated_at: props.updated_at,
+    });
+    props.is_active = false;
+    props.barcode = "123456789";
+    product = new Product(props);
+    expect(product.props).toStrictEqual({
+      name: "test",
+      description: "test description",
+      category: "picole",
+      barcode: "123456789",
+      is_active: false,
+      created_at: props.created_at,
+      updated_at: props.updated_at,
+    });
   });
 });
--- src/product/domain/entities/product.ts
+++ src/product/domain/entities/product.ts
@@ -10,32 +10,55 @@ export type ProductProps = {
 
 export class Product {
   constructor(public readonly props: ProductProps) {
-    this.props.is_active = this.props.is_active ?? true;
+    this.isActive = this.props.is_active ?? true;
+    this.barcode = this.props.barcode ?? "";
     this.props.created_at = this.props.created_at ?? new Date();
-    this.props.updated_at = this.props.updated_at ?? new Date();
+    this.updatedAt = this.props.updated_at ?? new Date();
   }
 
   get name(): string {
     return this.props.name;
   }
+  private set name(value: string) {
+    this.props.name = value;
+  }
 
   get description(): string {
     return this.props.description;
+  }
+  private set description(value: string) {
+    this.props.description = value;
   }
 
   get category(): string {
     return this.props.category;
   }
+  private set category(value: string) {
+    this.props.category = value;
+  }
 
   get barcode(): string {
     return this.props.barcode ?? "";
+  }
+  private set barcode(value: string) {
+    this.props.barcode = value ?? "";
   }
 
   get isActive() {
     return this.props.is_active;
   }
+  private set isActive(value: boolean) {
+    this.props.is_active = value ?? true;
+  }
 
   get createdAt() {
     return this.props.created_at;
+  }
+
+  get updatedAt() {
+    return this.props.updated_at;
+  }
+  private set updatedAt(value: Date) {
+    this.props.updated_at = value ?? new Date();
   }
 }
