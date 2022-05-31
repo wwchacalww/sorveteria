f546b30878d07ce6460486dcb53e98dcf7c3778c
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-05-31 17:02:46 -0300

    Validator-Rules atualizado e testado

--- src/@seedwork/domain/validators/validator-rules.ts
+++ src/@seedwork/domain/validators/validator-rules.ts
@@ -1,29 +1,40 @@
-import ValidationError from "../../errors/validation.error";
+import ValidationError from "../errors/validation.error";
 export default class ValidatorRules {
   private constructor(private value: any, private property: string) {}
-  static value(value: any, property: string) {
+  static values(value: any, property: string) {
     return new ValidatorRules(value, property);
   }
   required(): this {
     if (this.value === undefined || this.value === null || this.value === "") {
-      throw new ValidationError(`${this.property} is required`);
+      throw new ValidationError(`The ${this.property} is required`);
     }
     return this;
   }
 
   string(): this {
-    if (typeof this.value !== "string") {
-      throw new ValidationError(`${this.property} must be a string`);
+    if (!isEmpty(this.value) && typeof this.value !== "string") {
+      throw new ValidationError(`The ${this.property} must be a string`);
     }
     return this;
   }
 
   maxLength(max: number): this {
-    if (this.value.length > max) {
+    if (!isEmpty(this.value) && this.value.length > max) {
       throw new ValidationError(
-        `${this.property} must be less than ${max} characters`
+        `The ${this.property} must be less or equal than ${max} characters`
       );
     }
     return this;
   }
+
+  boolean(): this {
+    if (!isEmpty(this.value) && typeof this.value !== "boolean") {
+      throw new ValidationError(`The ${this.property} must be a boolean`);
+    }
+    return this;
+  }
+}
+
+function isEmpty(value: any) {
+  return value === undefined || value === null;
 }
