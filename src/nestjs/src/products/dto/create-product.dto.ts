a1e8de82cf3eb5154eac1ac6412a15bc8dd1e003
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-10 19:45:03 -0300

    Product Use cases injetados no controller do nestjs

--- src/nestjs/src/products/dto/create-product.dto.ts
+++ src/nestjs/src/products/dto/create-product.dto.ts
@@ -1,1 +1,10 @@
-export class CreateProductDto {}
+import Barcode from '@core/sorveteria-hakuna/dist/@seedwork/domain/value-objects/barcode.vo';
+import { CreateProductUseCase } from '@core/sorveteria-hakuna/product/application';
+
+export class CreateProductDto implements CreateProductUseCase.Input {
+  name: string;
+  description: string;
+  category: string;
+  barcode?: Barcode;
+  is_active?: boolean;
+}
--- src/nestjs/src/products/products.module.ts
+++ src/nestjs/src/products/products.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { CreateProductUseCase } from '@core/sorveteria-hakuna/product/application';
+import {
+  CreateProductUseCase,
+  GetProductUseCase,
+  ListProductsUseCase,
+  UpdateProductUseCase,
+  DeleteProductUseCase,
+} from '@core/sorveteria-hakuna/product/application';
 import { ProductInMemoryRepository } from '@core/sorveteria-hakuna/product/infra';
 import ProductRepository from '@core/sorveteria-hakuna/dist/product/domain/repository/product.repository';
 
@@ -17,6 +23,34 @@ import ProductRepository from '@core/sorveteria-hakuna/dist/product/domain/repos
       provide: CreateProductUseCase.UseCase,
       useFactory: (productRepo: ProductRepository.Repository) => {
         return new CreateProductUseCase.UseCase(productRepo);
+      },
+      inject: ['ProductRepository'],
+    },
+    {
+      provide: GetProductUseCase.UseCase,
+      useFactory: (productRepo: ProductRepository.Repository) => {
+        return new GetProductUseCase.UseCase(productRepo);
+      },
+      inject: ['ProductRepository'],
+    },
+    {
+      provide: ListProductsUseCase.UseCase,
+      useFactory: (productRepo: ProductRepository.Repository) => {
+        return new ListProductsUseCase.UseCase(productRepo);
+      },
+      inject: ['ProductRepository'],
+    },
+    {
+      provide: UpdateProductUseCase.UseCase,
+      useFactory: (productRepo: ProductRepository.Repository) => {
+        return new UpdateProductUseCase.UseCase(productRepo);
+      },
+      inject: ['ProductRepository'],
+    },
+    {
+      provide: DeleteProductUseCase.UseCase,
+      useFactory: (productRepo: ProductRepository.Repository) => {
+        return new DeleteProductUseCase.UseCase(productRepo);
       },
       inject: ['ProductRepository'],
     },
